3cfe09cc4f4bd8ccd9623bcf63a7a3504d3a76f9
Henry Cadavid <henrycadavidc@gmail.com>
2023-03-25 15:27:46 -0500

    aprendiendo mas

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const fn = () => {
-    const miString = "soy un string!"
-    let b = miString + "muy feliz"
-    console.log(b)
+function fn() {
+  const miString = "soy un string!";
+  let b = miString + " muy feliz";
+  console.log(b);
+  console.log("Hola Mundo utilizando snippet clg para console.log() ");
 }
 
-fn()
+fn();
 
 const clickeado = () => {
-    let a = "chanchito feliz"
+  let a = "chanchito feliz";
 
-    console.log(a)
-}
+  console.log(a);
+};
